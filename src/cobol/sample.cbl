704e29ba6408ac636c5c0d5c32860d2b161235f9
pbenichou <45391932+pbenichou@users.noreply.github.com>
2018-11-27 15:46:55 +0100

    Create sample.cbl

---
+++ src/cobol/sample.cbl
@@ -0,0 +1,5 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       PROCEDURE DIVISION.
+           DISPLAY 'Mon programme COBOL dans un conteneur Docker !'.
+           STOP RUN.
